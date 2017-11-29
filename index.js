b54916df57cb97344144e1f552b308123f84073a
HackJack <jack03113@gmail.com>
2017-11-29 17:24:36 +0100

    Catch push event on kb-dev/website event

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const xhub = require("express-x-hub");
+const {exec} = require('child_process');
 const settings = require('./settings');
 
 const port = "3001";
 const host = "localhost";
 
-app.use(xhub({ algorithm: "sha1", secret: settings.xhubSecret }));
+app.use(xhub({algorithm: "sha1", secret: settings.xhubSecret}));
 
 app.use(bodyParser.json());
 
-let server = app.listen(port, host, function() {
+let server = app.listen(port, host, function () {
     console.log(
         server.address().address,
         server.address().port
     );
 });
 
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
     if (!req.isXHubValid()) {
         res.status(400).send("Invalid X-Hub Request");
         console.error("Secret key is invalid");
@@ -30,12 +31,22 @@ app.post("/", function(req, res) {
     switch (command) {
         case "push":
             let repo = req.body.repository.full_name;
-            switch (repo){
+            switch (repo) {
                 case 'kb-dev/webhook':
-                    res.status(400).send("Push received");
+                    res.status(200).send("Push received on webhook");
+                    break;
+                case 'kb-dev/website':
+                    exec('cd /var/www/kbdev && git pull', (err) => {
+                        if (err) {
+                            res.status(500).send(`Push received on website with error ${err}`);
+                            return;
+                        } else {
+                            res.status(200).send("Push received on website and pull");
+                        }
+                    });
                     break;
                 default:
-                    res.status(400).send("Push for "+repo+" is not configured.");
+                    res.status(400).send("Push for " + repo + " is not configured.");
             }
             break;
 
